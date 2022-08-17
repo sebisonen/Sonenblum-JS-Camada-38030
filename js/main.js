a0d06df9a4f829ddc72f79f3bd9f64ccbe93fc60
sebisonen <sebisonenblum@gmail.com>
2022-08-17 20:42:47 -0300

    Incorporar eventos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-// -----------------DOM Y EVENTS----------------
+//Base de datos
+class User {
+    constructor(usuario, contraseña, nombreCompleto) {
+        this.nombreCompleto = nombreCompleto
+        this.usuario = usuario
+        this.contraseña = contraseña
+        this.agenda =[]
+    }
+}
+let users = []
+users.push(new User("Sebastian Sonenblum", "sebisonen", "12345"))
 
 // Elementos HTML
-
 const mesDiv = document.querySelector(`#mes-titulo`)
 const añoDiv = document.querySelector(`#año-titulo`)
 const botonSiguiente = document.querySelector(`#botonSiguiente`)
 const botonAtras = document.querySelector(`#botonAtras`)
 const botonHoy = document.querySelector(`#botonHoy`)
 const fechasCalendario = document.querySelector(`#fechasCalendario`)
+const pestañaEvento = document.querySelector(`#pestañaEvento`)
+
 // Snippets
     // Me da el nombre entero de cualquier mes
     function monthName(fecha){
@@ -36,20 +47,13 @@ const fechasCalendario = document.querySelector(`#fechasCalendario`)
         return copiaDeFecha.getDay(copiaDeFecha.setDate(daysInMonth(copiaDeFecha)))
     }
 // 
-// FUNCIONES DEL PROYECTO
-const hoy = new Date() 
-function calendario(copiaHoy){
-    mesDiv.innerText = `${monthName(copiaHoy).toUpperCase()}`
-    añoDiv.innerText = `${copiaHoy.getFullYear()}`
-    fechasCalendario.innerHTML=``
-    crearCuadricula(copiaHoy)
-    pintarHoy(hoy)
-}
 
+// FUNCIONES DEL PROYECTO
+const hoy = new Date()
 function inicio(){
     let copiaHoy = new Date(hoy.valueOf())
     let mesActual = copiaHoy.getMonth()
-    calendario(copiaHoy)
+    calendario(copiaHoy)//Imprimo el calendario en pantalla en base al dia de hoy
     botonHoy.addEventListener("click",()=>{
         copiaHoy = new Date(hoy.valueOf())
         mesActual = hoy.getMonth()
@@ -77,10 +81,28 @@ function inicio(){
     })    
 }
 
-// Crear el calendario especifico para cada mes y año
+//Hago dinamico el contenido de lo que se ve en pantalla
+function calendario(copiaHoy){
+    mesDiv.innerText = `${monthName(copiaHoy).toUpperCase()}`
+    añoDiv.innerText = `${copiaHoy.getFullYear()}`
+    fechasCalendario.innerHTML=``
+    crearCuadricula(copiaHoy)
+    resaltarHoy(hoy)
+}
+
+// Darle un detalle al dia actual
+function resaltarHoy(fecha){
+    const diasCalendario = document.querySelectorAll(`.diaCalendario`)
+    diasCalendario.forEach((div)=>{
+        if(fecha.valueOf()==div.id){
+            div.classList.add("diaActual"); 
+    }})
+}
+
+// Crear el calendario especifico segun las particularidades de cada mes y año
 function crearCuadricula(fecha){
     fecha.setDate(1) //Importante: Si la fecha fuese 31 de enero, cuando quiera crear un calendario para el mes siguiente no voy a poder porque no hay 31 de feb, con esto elimino el dia actual como referencia.
-    const copiaFecha = new Date(fecha.valueOf())// Creo una segunda variable con el dia de hoy igual a la primera para poder trabajar sin que me pise los valores de la original
+    const copiaFecha = new Date(fecha.valueOf())// Creo una segunda variable con el dia de hoy que sea igual a la primera: para poder trabajar sin que me pise los valores de la original
     let cuadradosPrevios = primerDia(fecha)
     let cuadradosPosteriores = 6 - ultimoDia(fecha)
     for(let i = 0, j = 1; i<daysInMonth(fecha)+cuadradosPrevios+cuadradosPosteriores; i++, j++){
@@ -89,149 +111,100 @@ function crearCuadricula(fecha){
                 copiaFecha.setDate(1)
                 j=1
                 copiaFecha.setMonth(fecha.getMonth()+1)
-                pintarHoy(copiaFecha)
+                
             }
             copiaFecha.setDate(j)
         }
         else{// Para los casos que hay que crear cuadros antes   
             copiaFecha.setDate(1)
             copiaFecha.setMonth(fecha.getMonth()-1)
-            copiaFecha.setDate(parseInt(daysInMonth(copiaFecha))-cuadradosPrevios+j) //Esto asigna la fecha que se pinta primero en el calendario(de las que pertenecen al mes anterior y van en la misma fila en el calendario).
-            if(i-cuadradosPrevios==primerDia(copiaFecha)){//Cuando terminé de crear los dias previos al primer dia
-                copiaFecha.setDate(j-cuadradosPrevios)
+            copiaFecha.setDate(parseInt(daysInMonth(copiaFecha))-cuadradosPrevios+j) //Esto me dice cual es el valor del primer cuadrado a pintar.
+            if(i-cuadradosPrevios==primerDia(copiaFecha)){//Cuando terminé de crear los dias previos al primer dia:
+                copiaFecha.setDate(j-cuadradosPrevios)//Reinicio el valor del dia que voy a crear
                 copiaFecha.setMonth(+1)
             }
         }
+        //Creo los div
         let div = document.createElement("div");
 		div.innerHTML = `<div id="${copiaFecha.valueOf()}" class="diaCalendario border vw-10">${copiaFecha.getDate()}</div>`
 		fechasCalendario.appendChild(div);
     }
+    eventoToggle()//Abrir ventana de agendar un evento. NOTA: Si o si se inicializa aca. Sino queda por fuera de los eventos
+    
 }
-// Pintar el dia actual
-function pintarHoy(fecha){
-    const diasCalendario = document.querySelectorAll(`.diaCalendario`)
-    diasCalendario.forEach((div)=>{
-        if(fecha.valueOf()==div.id){
-            div.classList.add("diaActual"); 
-}})}
 
-// Crear un evento
+// Cuando clickeo un div que se visualice el menu para agendar un evento
+function eventoToggle(){ //Se activa cuando clickeo una fecha especifica
+    const diasCalendario = document.querySelectorAll(`.diaCalendario`)
+    diasCalendario.forEach((div) =>{
+        div.addEventListener("click", (e)=>{//Por cada dia del calendario creo la posibilidad de agendar un nuevo evento en aquel que clickee
+            // Formateos de fecha para poder poner placeholders en forma de values
+            let date = new Date(parseInt(div.id));
+            let valueEventoDia = new Date(parseInt(div.id)).toISOString().split('T')[0]
+            let value= date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+            date.setHours(date.getHours() + 1)
+            let value2 = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+
+            pestañaEvento.style.display = "block"//Que se vea el menu
+            pestañaEvento.innerHTML = `
+                <div id="evento" class="d-flex flex-column justify-content-center w-100 h-100 p-3 gap-3">
+                    <h2> Agende un evento </h2>
+                    <input type="text" id="eventoNombre" placeholder="Ingrese un titulo">
+                    <input type="date" id="eventoDia" value="${valueEventoDia}">
+                    <div class="d-flex">
+                        <input type="time" id="eventoDesde" value="${value}">
+                        <input type="time" id="eventoHasta" value="${value2}">
+                    </div>
+                    <input id="eventoDetalle" type="text" placeholder="Agregar informacion">
+                    <div class="d-flex gap-1">
+                        <button id="agregarEvento" class="boton rounded">Agregar</button>
+                        <button id="cancelarEvento" class="btn-dark rounded">Cancelar</button>
+                    </div>
+                </div>
+            `
+            agendarEvento(e)
+        })
+    })
+  
+}
+
+function agendarEvento(e){
+    // Variables DOM de la ventana toggle de eventoToggle()
+    const eventoNombre = document.querySelector(`#eventoNombre`)
+    const eventoDesde = document.querySelector(`#eventoDesde`)
+    const eventoHasta = document.querySelector(`#eventoHasta`)
+    const eventoDetalle = document.querySelector(`#eventoDetalle`)
+    const cancelarEvento = document.querySelector(`#cancelarEvento`)
+    const agregarEvento = document.querySelector(`#agregarEvento`)
+
+        cancelarEvento.addEventListener("click", ()=>{//Si no quiero agregar, cierro la ventana
+            pestañaEvento.style.display = "none"
+        })
+        agregarEvento.addEventListener("click", ()=>{
+            // Manipulo los datos del html para armar la fecha desde (lio por los tipos de datos y conversiones del objeto Date)
+            let desde=  new Date(parseInt(e.target.id))
+            let horasDesde= parseInt(eventoDesde.value.substring(0,2))
+            let minutosDesde= parseInt(eventoDesde.value.substring(3))
+            desde.setHours(horasDesde,minutosDesde,0)
+            // Manipulo los datos del html para armar la fecha hasta (lio por los tipos de datos y conversiones del objeto Date)
+            let hasta = new Date(parseInt(e.target.id))
+            let horasHasta= parseInt(eventoHasta.value.substring(0,2))
+            let minutosHasta= parseInt(eventoHasta.value.substring(3))
+            hasta.setHours(horasHasta,minutosHasta,0)
+            
+            console.log(desde,hasta)
+            // Agrego el nuevo evento a la agenda
+            let eventoCreado = {
+                tíulo: eventoNombre.value,
+                desde: desde,
+                hasta:hasta,
+                informacion: eventoDetalle.value,
+            }
+            users[0].agenda.push(eventoCreado)
+            console.log(users[0].agenda)
+            pestañaEvento.style.display = "none" //Hago que se cierre la ventana
+})}
 
 // Inicializacion de funciones
 inicio()
-
-
-
-
-// ----------------------------ENTREGAS PASADAS------------------------
-// Simulador - Agenda grupal para coordinacion de citas
-// FUNCIONES Y VARIABLES
-class User {
-    constructor(usuario, contraseña, nombreCompleto) {
-        this.nombreCompleto = nombreCompleto
-        this.usuario = usuario
-        this.contraseña = contraseña
-        this.agenda ={}
-    }
-}
-
-let users = []
-
-// Genero la base de datos simulada
-
-function cargarUsuarios(){
-    let seguirCargando= true
-    let i = 1
-    while (seguirCargando) {
-        let nombreCompleto = validarDato(`Ingrese el nombre y el apellido del usuario N°${i}`)
-        let usuario = validarDato(`Escriba el usuario con el que ${nombreCompleto} quiere registrarse`)
-        alert("Su contraseña por este momento es 123456789 para evitarle llenar campos innecesarios")
-        let contraseña = 123456789
-        let nuevoUsuario = new User (usuario, contraseña, nombreCompleto)
-        users.push(nuevoUsuario)
-        console.log(`La cuenta de ${usuario} ha sido creada con éxito` )
-        seguirCargando = confirm("¿Desea cargar otro usuario más para cruzar agendas?")
-        i++
-        
-    }
-    console.clear()
-    
-}
-// Funcion de uso futuro y util para ir debuggeando
-function visualizarUsuarios(array){
-    array.forEach((el =>{
-        console.log(el.usuario)
-    }))
-}
-
-// Doy la opcion de eliminar algun usuario
-function eliminarUsuario(array){
-    let eliminar = confirm("Desea eliminar algun usuario?")
-    while(eliminar){
-        console.log(`Estos son los usuarios que ha cargado:`)
-        visualizarUsuarios(users)
-        let input = prompt("Escriba el nombre del usuario que quiere borrar")
-        let buscar = array.find((el) => el.usuario === input)
-        let indice = array.indexOf(buscar)
-        while (buscar==undefined){
-            alert(`No se ha encontrado el usuario ${input}`)
-            input = prompt("Escriba el nombre del usuario que quiere borrar")
-            buscar = array.find((el) => el.usuario === input)
-            indice = array.indexOf(buscar)
-        }
-        array.splice(indice,1)
-        alert(`Se ha borrado el usuario ${buscar.usuario}`)
-        console.clear()
-        if (users.length==0){
-            alert(`Ya no quedan usuarios cargados. Se le redirigirá al inicio`)
-            cargarUsuarios()
-        }
-        eliminar = confirm("¿Desea eliminar otro mas?")
-    }
-    
-}
-
-// Chequea que se haya escrito algo en todos los campos
-
-function validarDato(mensaje){
-    let dato = prompt(mensaje)
-    while(dato==null || dato==""){
-        alert(`El valor ingresado no es válido.`)
-        dato = prompt(mensaje)
-    }
-    return dato
-}
-
-
-// Cargar para cada usuario los horarios que tiene libre
-
-function cargarHorarios (){
-    for (const user of users){
-        console.log(`Agenda de ${user.nombreCompleto}\nUsuario:`,user.usuario,`Contraseña:`,user.contraseña)        
-        let cargarDia = true
-        let cargarHorario = true
-        // Cargo dias de la semana
-        while(cargarDia){
-            let dia = validarDato("Para que día de la semana quiere cargar horarios?")
-            user.agenda[`${dia}`] = []
-
-            while(cargarHorario){
-                let desde = validarDato(`Desde qué horario esta disponible ${user.nombreCompleto}?`)
-                let hasta = validarDato(`Hasta qué horario esta disponible ${user.nombreCompleto}?`)
-                let disponibilidad = [desde,hasta]
-                user.agenda[`${dia}`].push(disponibilidad)
-                console.log(`Estoy disponible el dia ${dia} desde las ${disponibilidad[0]}hs hasta las ${disponibilidad[1]}hs`)
-                cargarHorario = confirm(`¿Desea seguir cargando horarios para el dia ${dia}`)
-            }
-            cargarDia = confirm(`¿Desea seguir agregando horarios del usuario ${user.nombreCompleto}?`)
-            cargarHorario=true
-        }
-    }
-}
-
-
-
-// Ejecucion
-// cargarUsuarios()
-// eliminarUsuario(users)
-// cargarHorarios()
+eventoToggle()
